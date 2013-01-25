8e23e4a7f424537fca56cbf3bf0d90c9f8869ab2
James Proctor <jproctor@udel.edu>
2013-01-24 21:55:12 -0500

    trying out new sorting settings

--- GUI_PixelSorting.js
+++ GUI_PixelSorting.js
@@ -2,69 +2,175 @@
 
 var sortPixels = (function(){
 
-  var threshold, density, mode, numPixels, loopCount;
+  var threshold, mode, numPixels, loopCount;
 
-  function init(img, mode) {
-    threshold = 135;
-    density = 3;
-    mode = 0;
-    loopCount = 0;
+  var loop = function() {
+    if (this.vLoop >= this.imageStuff.height) {
+      console.log("vLoop = " + this.vLoop);
+      this.vLoop = 0;
+      this.threshold++;
+      if (this.threshold > 255) {
+        this.threshold = 0;
+      }
+//      this.mode++;
+//      if (this.mode > 2) {
+//        this.mode = 0;
+//      }
+    }
+
+    this.imageStuff.imageData = draw();
+    this.imageStuff.imageDataWrapper.data = this.imageStuff.imageData;
+    this.imageStuff.ctx.clearRect(0,0, this.imageStuff.width, this.imageStuff.height);
+    this.imageStuff.ctx.putImageData(this.imageStuff.imageDataWrapper, 0,0);
+    this.vLoop++;
+    //console.log('Completed a cycle.');
+  }
     
-    setup(img);
-    draw();
     
-    return canvas;
+  var init = function(img, mode, threshold) {
+    this.threshold = threshold;
+    //this.threshold = 135;
+    this.mode = mode;// || 1;
+    this.vLoop = 0;    
+    this.loopCount = 0;
+
+    this.canvas = setup_canvas(img);
+    this.imageStuff = setup(img);
+    
+    this.imageStuff.imageData = draw();
+
+    this.imageStuff.imageDataWrapper.data = this.imageStuff.imageData;
+    this.imageStuff.ctx.clearRect(0,0, this.imageStuff.width, this.imageStuff.height);
+    this.imageStuff.ctx.putImageData(this.imageStuff.imageDataWrapper, 0,0);
+    return {
+      canvas: this.canvas,
+      fxn: loop
+    };
   }
   
-  function setup(img) {
+  function setup_canvas(img) {
     canvas = document.createElement('canvas');
-    width = canvas.width = img.naturalWidth;
-    height = canvas.height = img.naturalHeight;
-    numPixels = width*height;
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    return canvas;
+  }
+
+  function setup(img) {
+    width = this.canvas.width;
+    height = this.canvas.height;
+    numPixels = width*height*4;
     
     ctx = canvas.getContext('2d');
     ctx.drawImage(img,0,0);
     
-    // document.body.appendChild(img);
-    // document.body.appendChild(canvas);
     imageDataWrapper = ctx.getImageData(0, 0, width, height);
     imageData = imageDataWrapper.data;
+    var imageStuff = {
+      width: width,
+      height: height,
+      numPixels: numPixels,
+      ctx: ctx,
+      imageData: imageData,
+      imageDataWrapper: imageDataWrapper
+    };
+    return imageStuff;
   }
-  
+ 
+  function setPixel(imageData, offset, pixel) {
+    for (var i=0; i < 3; i++) {
+      imageData[offset + i] = pixel[i];
+    }
+    return imageData;
+  }
+
+  function getPixel(imageData, offset) {
+    var pixel = new Array();
+    for (var i=0; i < 3; i++) {
+      pixel[i] = imageData[offset + i];
+    }
+    return pixel;
+  }
+ 
   function draw() {
-    for (w = 0; w < numPixels; w++){
-        switch(mode) {
+    // ugh, these are here to make things a *bit* more readable and less
+    // verbose, should eventually break some of the if statements into
+    // functions i think
+    var loopCount = this.loopCount;
+    var threshold = this.threshold;
+    var numPixels = this.imageStuff.numPixels;
+    var imageData = this.imageStuff.imageData;
+    var width = this.imageStuff.width;
+    var height = this.imageStuff.height;
+    while (loopCount < numPixels){
+        switch(this.mode) {
           case 0:
-            if (w > img.width) { 
-              if (getPixelBrightness(img.pixels[w]) > threshold) {
-                img.pixels[w] = img.pixels[w-img.width];
+            if (loopCount > width) { 
+              if (getPixelBrightness(loopCount) > threshold) {
+                imageData = setPixel(imageData, loopCount, getPixel(imageData, loopCount - width));
+//              if (getPixelBrightness(img.pixels[loopCount]) > threshold) {
+//                img.pixels[loopCount] = img.pixels[loopCount-img.width];
+                
               }
             }
             break;
           case 1:
-            if (w > 0) {
-              if (getPixelBrightness(img.pixels[w]) > threshold) {
-                img.pixels[w] = img.pixels[w-1];
+            if (loopCount > height) {
+              if (getPixelBrightness(loopCount) > threshold) {
+                imageData = setPixel(imageData, loopCount, getPixel(imageData, loopCount -1));
+//              if (getPixelBrightness(img.pixels[loopCount]) > threshold) {
+//                img.pixels[loopCount] = img.pixels[loopCount-1];
               }
             }
             break;
           case 2:
-            if (w > img.width) { 
-              if (getPixelBrightness(img.pixels[w]) > threshold) {
-                img.pixels[w-img.width] = img.pixels[w];
-                //img.pixels[w-img.width] = int(abs(sin(w)*w));
+            if (loopCount > width) { 
+//              var test = getPixelBrightness(loopCount);
+              if (getPixelBrightness(loopCount) > threshold) {
+        // Here's a whole bunch of things I tried to mess with the image data
+        // I'm thinking we could replace the existing 'modes' with some of these.
+
+          //Coolest looking
+                //Color Separation +
+                  //imageData = setPixel(imageData, (loopCount+ height), getPixel(imageData, loopCount));
+                //Color Separation -
+                  imageData = setPixel(imageData, (loopCount- height), getPixel(imageData, loopCount));
+                //washed out and color shifted
+                  //imageData = setPixel(imageData, (loopCount- threshold), getPixel(imageData, loopCount));
+
+          //Intersting
+                //snow crash!
+                  //imageData = setPixel(imageData, (loopCount++ ), getPixel(imageData, loopCount));
+                //seizure warning
+                  //imageData = setPixel(imageData, (loopCount-- ), getPixel(imageData, loopCount));
+                //Granular
+                  //imageData = setPixel(imageData, (loopCount/ .8), getPixel(imageData, loopCount));
+
+          //Moving
+                //slide everything left
+                  //imageData = setPixel(imageData, (loopCount- 4), getPixel(imageData, loopCount));
+                //emboss effect
+                  //imageData = setPixel(imageData, (loopCount- 3), getPixel(imageData, loopCount));
+                //Green and Purple
+                  //imageData = setPixel(imageData, (loopCount- 2), getPixel(imageData, loopCount));
+
+              //Original working JS 
+                  //imageData = setPixel(imageData, (loopCount- width), getPixel(imageData, loopCount));
+                //Processing version
+                  //img.pixels[loopCount-img.width] = int(abs(sin(loopCount)*loopCount));
               }
             }
             break;       
         }
+        loopCount = loopCount + 4;
     }
-    
+    /*
     if (loopCount++ >= numPixels) {
       ctx.putImageData(imgeDataWrapper,0,0);
     } else {
-      //draw();
+      draw();
     }
-    
+    */
+    return imageData;
   }
   
   function getPixelBrightness(x) {
@@ -73,7 +179,7 @@ var sortPixels = (function(){
     var g = imageData[offset + 1];
     var b = imageData[offset + 2];
     // HSL - lightness:
-    // return (Math.max(r,g,b) + Math.min(r,g,b)) / 2
+    //return (Math.max(r,g,b) + Math.min(r,g,b)) / 2
     // HSV - value:
     return Math.max(r,g,b) / 255 * 100;
   }
